f6115332c87af4715b5c2747b8e445cd0e7bd552
Daniel Kraemer <dkraemer@dkross.org>
2023-09-21 05:24:29 +0200

    ConsoleApp: Created template

---
+++ templates/content/ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleApp1;
+
+file static class Program
+{
+    private static async Task Main(string[] args)
+    {
+        await Console.Out.WriteLineAsync("Hello, World!");
+    }
+}
--- tools/test.ps1
+++ tools/test.ps1
@@ -1,17 +1,24 @@
 Set-Location $PSScriptRoot
 dotnet new install ../templates
 
+# Solution Test
 dotnet new dkross.sln -o temp -n Temp --allow-scripts yes `
     --git-init `
     --git-remote-url https://github.com/dkross/git-test.git
 
+# ClassLibrary Test
 Set-Location temp
 dotnet new dkross.lib -o src/lib -n DKrOSS.Lib
 dotnet sln add src/lib/DKrOSS.Lib.csproj
 
+# Class Test
 Push-Location src/lib
 dotnet new dkross.class -n Settings
 Pop-Location
+
+# ConsoleApp Test
+dotnet new dkross.console -o src/ConsoleApp -n DKrOSS.ConsoleApp
+dotnet sln add src/ConsoleApp/DKrOSS.ConsoleApp.csproj
 
 Read-Host "Press ENTER to start cleanup..."
 Set-Location $PSScriptRoot
