2d8158a831c828fccad0feb16b7b04a4aa3289d7
Daniel Kraemer <dkraemer@dkross.org>
2023-09-21 02:52:26 +0200

    Added Class template

--- tools/test.ps1
+++ tools/test.ps1
@@ -6,9 +6,15 @@ dotnet new dkross.sln -o temp -n Temp --allow-scripts yes -g -gi https://github.
 Set-Location temp
 dotnet new dkross.lib -o src/lib -n DKrOSS.Lib
 dotnet sln add src/lib/DKrOSS.Lib.csproj
-Invoke-Item Temp.sln
 
-Read-Host "Press any key to continue with cleanup..."
+Push-Location src/lib
+dotnet restore
+dotnet new dkross.class -n Settings
+Pop-Location
+
+Invoke-Item .
+
+Read-Host "Press ENTER to start cleanup..."
 Set-Location $PSScriptRoot
 Remove-Item -Force -Recurse temp
 dotnet new uninstall ../templates
