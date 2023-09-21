a4808200494ac31a5e2ba197ec8d734fb3912942
Daniel Kraemer <dkraemer@dkross.org>
2023-09-21 17:04:50 +0200

    Solution: Added generic template variant

--- tools/test.ps1
+++ tools/test.ps1
@@ -1,13 +1,19 @@
 Set-Location $PSScriptRoot
 dotnet new install ../templates
 
+# Solution Test (generic)
+dotnet new dkross.sln -o temp/generic -n Temp --allow-scripts yes `
+    --git-init `
+    --git-remote-url https://github.com/dkross/git-test.git `
+    --generic
+
 # Solution Test
-dotnet new dkross.sln -o temp -n Temp --allow-scripts yes `
+dotnet new dkross.sln -o temp/dotnet -n Temp --allow-scripts yes `
     --git-init `
     --git-remote-url https://github.com/dkross/git-test.git
 
 # ClassLibrary Test
-Set-Location temp
+Set-Location temp/dotnet
 dotnet new dkross.lib -o src/lib -n DKrOSS.Lib
 dotnet sln add src/lib/DKrOSS.Lib.csproj
 
