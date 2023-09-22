eb85682cbd2be46c28056ef8b77bca2dc3ebc324
Daniel Kraemer <dkraemer@dkross.org>
2023-09-23 00:27:39 +0200

    Minor improvements (#4)
    * Solution: Sign git commits with GPG
    * ClassLibrary: Commit project after creation
    * ConsoleApp: Commit project after creation

--- templates/content/Solution/.postAction.git.ps1
+++ templates/content/Solution/.postAction.git.ps1
@@ -1,5 +1,6 @@
 Remove-Item -Force $PSCommandPath
 git init
+git config commit.gpgsign true
 git add .
 git commit -m "Initial commit"
 ##EnableGitRemoteAdd##git remote add ##GitRemoteAlias## ##GitRemoteUrl##
