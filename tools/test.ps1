cdd9f583b3a5b192f8b4baccf1f7ff5fd1b9c162
Daniel Kraemer <dkraemer@dkross.org>
2023-09-22 23:08:15 +0200

    test.ps1: Added build step for projects

--- tools/test.ps1
+++ tools/test.ps1
@@ -21,11 +21,15 @@ dotnet new dkross.lib -o src/lib -n DKrOSS.Lib --allow-scripts yes `
 Push-Location src/lib
 dotnet new dkross.class -n Settings
 dotnet new dkross.interface -n ISettings
+dotnet build -p:TreatWarningsAsErrors=true
 Pop-Location
 
 # ConsoleApp Test
 dotnet new dkross.console -o src/ConsoleApp -n DKrOSS.ConsoleApp --allow-scripts yes `
     --solution-path ../../Temp.sln
+Push-Location src/ConsoleApp
+dotnet build -p:TreatWarningsAsErrors=true
+Pop-Location
 
 Read-Host "Press ENTER to start cleanup..."
 Set-Location $PSScriptRoot
