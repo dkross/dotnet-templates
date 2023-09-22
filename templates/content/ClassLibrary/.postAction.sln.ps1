0c6e227dada513457f3d90006159b578dae09275
Daniel Kraemer <dkraemer@dkross.org>
2023-09-22 21:53:53 +0200

    ClassLibrary: Updated for improved MSBuild files

---
+++ templates/content/ClassLibrary/.postAction.sln.ps1
@@ -0,0 +1,2 @@
+Remove-Item -Force $PSCommandPath
+dotnet sln ##SolutionPath## add ClassLib1.csproj --in-root
--- tools/test.ps1
+++ tools/test.ps1
@@ -14,8 +14,8 @@ dotnet new dkross.sln -o temp/dotnet -n Temp --allow-scripts yes `
 
 # ClassLibrary Test
 Set-Location temp/dotnet
-dotnet new dkross.lib -o src/lib -n DKrOSS.Lib
-dotnet sln add src/lib/DKrOSS.Lib.csproj
+dotnet new dkross.lib -o src/lib -n DKrOSS.Lib --allow-scripts yes `
+    --solution-path ../../Temp.sln
 
 # Item Templates Test
 Push-Location src/lib
@@ -23,9 +23,9 @@ dotnet new dkross.class -n Settings
 dotnet new dkross.interface -n ISettings
 Pop-Location
 
-# ConsoleApp Test
-dotnet new dkross.console -o src/ConsoleApp -n DKrOSS.ConsoleApp
-dotnet sln add src/ConsoleApp/DKrOSS.ConsoleApp.csproj
+# # ConsoleApp Test
+# dotnet new dkross.console -o src/ConsoleApp -n DKrOSS.ConsoleApp
+# dotnet sln add src/ConsoleApp/DKrOSS.ConsoleApp.csproj
 
 Read-Host "Press ENTER to start cleanup..."
 Set-Location $PSScriptRoot
