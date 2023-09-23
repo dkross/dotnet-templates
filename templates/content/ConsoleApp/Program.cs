e6890a85e997cb85ee3f097bf858a4036bed27d4
Daniel Kraemer <dkraemer@dkross.org>
2023-09-23 05:49:20 +0200

    Console: Revert change from (#3)
    CS1998 is not suppressed anymore.

--- templates/content/ConsoleApp/Program.cs
+++ templates/content/ConsoleApp/Program.cs
@@ -2,10 +2,8 @@
 
 file static class Program
 {
-#pragma warning disable CS1998 // This async method lacks 'await' operators and will run synchronously.
     private static async Task Main(string[] args)
-#pragma warning restore CS1998
     {
-
+        await Console.Out.WriteLineAsync("Hello, world!");
     }
 }
